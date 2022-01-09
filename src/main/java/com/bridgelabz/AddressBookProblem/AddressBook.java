0d90128cce5a91382be0f4dad2e9d74c8efbe0d5
lokesh <kottelokesh789@gmail.com>
2022-01-09 11:03:18 +0530

    ADDED: EachAddressBookHasAUniqueName

--- src/main/java/com/bridgelabz/AddressBookProblem/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBookProblem/AddressBook.java
@@ -5,9 +5,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class AddressBook {
-Scanner sc = new Scanner(System.in);
-	
-	
+	Scanner sc = new Scanner(System.in);
 	
 	public void editContact(Map<String, AddressBookDetails> addressBook, AddressBookDetails information)
 	{
@@ -53,63 +51,77 @@ Scanner sc = new Scanner(System.in);
 	public void addContact()
 	{
 		Scanner sc = new Scanner(System.in);
-    	Map<String, AddressBookDetails> addressBook = new HashMap<>();
-    	String ans = "Y";
-    	AddressBookDetails information = null;
-    	while(ans.equals("Y"))
-    	{
-        
-            //----------------Strings----------------------//
-            System.out.println("Enter first name = ");
-            String firstName = sc.next();
-            System.out.println("Enter Last name = ");
-            String lastName = sc.next();
-            System.out.println("Enter Address = ");
-            String address = sc.next();
-            System.out.println("Enter City = ");
-            String subCity = sc.next();
-            System.out.println("Enter  State = ");
-            String state = sc.next();
-            System.out.println("Enter  Email= ");
-            String email = sc.next();
-            
-            //-------------int & long---------------//
-            System.out.println("Enter zip code = ");
-            int Zip = sc.nextInt();
-            System.out.println("Enter phone Number = ");
-            long PhoneNumber = sc.nextLong();
-            
-            information = new AddressBookDetails(firstName,lastName,address,subCity,state,email,Zip,PhoneNumber);
-	
-	        addressBook.put(firstName,information);
-	        for(String AddressBookDetails: addressBook.keySet())
-	        {
-	            System.out.println("firstName = " + information.getFirstName() +
-	                    "\nlast Name = "+ information.getLastName() +
-	                    "\naddress = "+ information.getAddress() +
-	                    "\ncity = "+ information.getCity() +
-	                    "\nstate = "+ information.getState() +
-	                    "\nemail = "+ information.getEmail() +
-	                    "\nZip = "+ information.getZipCode() +
-	                    "\nPhoneNumber = "+ information.getPhoneNumber());
-        	};
-        	
-        	System.out.println("Do you want to add new contact (Y-yes/N-no): ");
-        	ans = sc.next();
-        }
-    		System.out.println("Edit Contact (press 1)"
-    				+ "Delete Contact (press 2)");
-    		int action = sc.nextInt();
-    		switch(action) {
-    			case 1: editContact(addressBook,information);
-    			break;
-    			
-    			case 2: deleteContact(addressBook,information);
-    			break;
-    			
-    			default: System.err.println("invalid input");
-    			break;
-    				
+		boolean cond = true;
+		while(cond) {
+			System.out.println("Enter the name of ur address book");
+			String nameOfAddressBook = sc.next();
+			Map<String, HashMap<String, AddressBookDetails>> primeAddressBook = new HashMap<>();
+			
+	    	Map<String, AddressBookDetails> addressBook = new HashMap<>();
+	    	primeAddressBook.put(nameOfAddressBook,(HashMap<String, AddressBookDetails>) addressBook);
+	    	String ans = "Y";
+	    	AddressBookDetails information = null;
+	    	System.out.println("--------Contacts in " + nameOfAddressBook + "---------");
+	    	while(ans.equals("Y"))
+	    	{
+	        
+	            //----------------Strings----------------------//
+	            System.out.println("Enter first name = ");
+	            String firstName = sc.next();
+	            System.out.println("Enter Last name = ");
+	            String lastName = sc.next();
+	            System.out.println("Enter Address = ");
+	            String address = sc.next();
+	            System.out.println("Enter City = ");
+	            String subCity = sc.next();
+	            System.out.println("Enter  State = ");
+	            String state = sc.next();
+	            System.out.println("Enter  Email= ");
+	            String email = sc.next();
+	            
+	            //-------------int & long---------------//
+	            System.out.println("Enter zip code = ");
+	            int Zip = sc.nextInt();
+	            System.out.println("Enter phone Number = ");
+	            long PhoneNumber = sc.nextLong();
+	            
+	            information = new AddressBookDetails(firstName,lastName,address,subCity,state,email,Zip,PhoneNumber);
+		
+		        addressBook.put(firstName,information);
+		        for(String AddressBookDetails: addressBook.keySet())
+		        {
+		            System.out.println("firstName = " + information.getFirstName() +
+		                    "\nlast Name = "+ information.getLastName() +
+		                    "\naddress = "+ information.getAddress() +
+		                    "\ncity = "+ information.getCity() +
+		                    "\nstate = "+ information.getState() +
+		                    "\nemail = "+ information.getEmail() +
+		                    "\nZip = "+ information.getZipCode() +
+		                    "\nPhoneNumber = "+ information.getPhoneNumber());
+	        	};
+	        	
+	        	System.out.println("Do you want to add new contact (Y-yes/N-no): ");
+	        	ans = sc.next();
+	        }
+	    		System.out.println("Edit Contact (press 1)"
+	    				+ "\nDelete Contact (press 2)"
+	    				+ "\\nCreat New Adressbook (press 3)");
+	    		int action = sc.nextInt();
+	    		switch(action) {
+	    			case 1: editContact(addressBook,information);
+	    			break;
+	    			
+	    			case 2: deleteContact(addressBook,information);
+	    			break;
+	    			
+	    			case 3: addContact();
+	    			break;
+	    			
+	    			default: System.err.println("invalid input");
+	    			break;
+	    				
     		}
+		}
 	}
+
 }
